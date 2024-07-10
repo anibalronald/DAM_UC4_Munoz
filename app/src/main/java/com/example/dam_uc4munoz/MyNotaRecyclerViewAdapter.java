a3f6f151bec12614ebb16eb5691937b81d1cb432
anibal muñoz <2113110302@untels.edu.pe>
2024-07-10 10:52:14 -0500

    Notas columnas muñoz

---
+++ app/src/main/java/com/example/dam_uc4munoz/MyNotaRecyclerViewAdapter.java
@@ -0,0 +1,64 @@
+package com.example.dam_uc4munoz;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+import com.example.dam_uc4munoz.databinding.FragmentItemBinding;
+
+import java.util.List;
+
+
+public class MyNotaRecyclerViewAdapter extends RecyclerView.Adapter<MyNotaRecyclerViewAdapter.ViewHolder> {
+
+    private final List<Nota> mValues;
+    private final NotasInteractionListener mListener;
+
+    public MyNotaRecyclerViewAdapter(List<Nota> notaList, NotasInteractionListener mListener) {
+        this.mValues = notaList;
+        this.mListener = mListener;
+    }
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+
+                .inflate(R.layout.fragment_item, parent, false);
+        return new ViewHolder(View);
+    }
+
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        //holder.mIdView.setText(mValues.get(position).id);
+       //holder.mContentView.setText(mValues.get(position).content);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final TextView mIdView;
+        public final TextView mContentView;
+        public Nota mItem;
+
+        public ViewHolder(FragmentItemBinding binding) {
+            super(binding.getRoot());
+            mIdView = binding.itemNumber;
+            mContentView = binding.content;
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+}
